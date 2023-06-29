d2dc812a4788b57258eea51aa965dd907d918043
SuvarnaRaju <smasipog@jcp.com>
2023-06-29 11:44:51 +0530

    first commit

---
+++ src/main/java/com/springboot/test/jenkins_github/JenkinsGithubApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.test.jenkins_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/test/jenkins_github/JenkinsGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.test.jenkins_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
